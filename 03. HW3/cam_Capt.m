5080e948dc86124f8da7d77edfdc627ffc17347e
alexandresoaresilva <alexandre.soares-da-silva@ttu.edu>
2018-11-03 00:29:05 -0500

    digit segmentation
    practically complete . Might need some closing applied to the fuzz digits.

--- 03. HW3/cam_Capt.m
+++ 03. HW3/cam_Capt.m
@@ -1,42 +1,113 @@
 clc, clear, close all
-
-filter=@(n)1/(n^2)*ones(n);
-
-%% webcam setup
+% webcam setup
 % webcamlist_conn = webcamlist;
 % webcam_connected = webcam(webcamlist_conn{1});
 % webcam_connected.Resolution ='640x480';
-% preview(webcam_connected);
+% 
+% webcam_connected.Resolution = '320x240';
+% prv = preview(webcam_connected);
+% prv.Parent.Visible = 'off';
+% prv.Parent.Parent.Parent.Position(1:2) = [0.6 .5];
+% prv.Parent.Visible = 'on';
 % prompt = 'Press enter to capture a frame';
 % x = input(prompt);
 % I = snapshot(webcam_connected);
-% I = rgb2gray(I);
+% delete(prv);
 
+%% load file
+% I = imread('fuzzy1_2.jpg');
+I = imread('small_Digits.jpg');
 
-I = imread('7.jpg');
-LPF = filter(13);
+if length(size(I)) > 2
+    I = rgb2gray(I);
+end
 
+%% procesing
+% I = imresize(I,[240 320]);
+[M, N] = size(I);
+LPF = avg_filt(2);
+% I1 = I;
+I1 = conv2(I,LPF,'valid');
+
+I_bin = ~imbinarize(uint8(I1),.4);
+for i=1:2
+    I_bin = medfilt2(I_bin);
+end
+%imshow()
 %I1 = preprocess(I)
-I1 = conv2(I,LPF,'same');
-%I1 = imgaussfilt(I,10);
-% I2 = wiener2(I1,[5 5]);
-% I2 = single(edge(I2,'Canny'));
+
+% I1 = double(I)-I1;
+% Iw1 = imgaussfilt(I,3);
+% Iw2 = wiener2(I1,[5 5]);
+% Iw2 = single(edge(I2,'Canny'));
 %% matlab nonuniform illumination
-[I2, I3, I4] = matlab_nonuni_illu_remov(I,6, 15);
-se = strel('disk',20);
-
-I5 = imclose(I4,se);
-
-I_cell = {I1, I2, I3, I4, I5};
+% [I2, I3, I4] = matlab_nonuni_illu_remov(I,7, 15);
+se = strel('disk',6);
+% I_bin = conv2(double(I_bin)*255,LPF,'valid');
+% I_bin = uint8(I_bin);
+% I_bin = imbinarize(I_bin,.5);
+I_binclosed = imclose(I_bin,se);
+I_cell = {I, I1, I_bin, I_binclosed};
+I_props = regionprops(I_binclosed);
 
 figure
-sub_plots(I_cell, length(I_cell));
+imshow(I_binclosed);
+hold on;
+digits = cellmat(0);
+j = 1;
+for i=1:length(I_props)
+    if I_props(i).Area > (M*N / 330)
+        if I_props(i).BoundingBox(4) > N/8
+            rect = rectangle('Position',I_props(i).BoundingBox,'EdgeColor','r','LineWidth',3);
+            
+            digit = imcrop(I_binclosed, I_props(i).BoundingBox);
+%             digits{i} = imcrop(I_binclosed, I_props(i).BoundingBox);
+            [m,n] = size(digit);
+            [max_x,i] = max([m,n]);            
+%           0  100
+            
+            digits{j} = pad_digit_img(digit ,ceil(max_x*1.2));
+            j = j + 1;
+        end
+    end
+end
 
-colormap gray;
+figure
+sub_plots(digits);
 
-function sub_plots(I_cell,n)
+
+%% functions
+function I = pad_digit_img(I,square_side)
+    [x,y] = size(I);
+    [x_pre, x_pos] = padding_pre_post_calc(x,square_side);
+    [y_pre, y_pos] = padding_pre_post_calc(y,square_side);
+
+    I = padarray(I,[x_pre y_pre],'pre');
+    I = padarray(I,[x_pos y_pos],'pos');
+
+
+    function [n_pre, n_pos] = padding_pre_post_calc(n,dim_size)
+        
+        n_is_not_even = rem(dim_size-n,2);
+        n_pre = (dim_size-n)/2;
+%         n_pre_is_not_even = rem(n_pre,2);
+        
+        if n_is_not_even && n_pre %n_pre not zero
+            n_pre = n_pre - 0.5;
+            n_pos = n_pre + 1;
+        else
+            n_pos = n_pre;
+        end
+        n_pos = double(n_pos);
+        n_pre = double(n_pre);
+    end
+end
+
+function sub_plots(I_cell)
+    n = length(I_cell);
+    no_of_plots_per_row = ceil(n/2);
     for i=1:n
-        subplot(2,3,i)
+        subplot(2,no_of_plots_per_row,i)
         imshow(I_cell{i},[]);
     end
 end
@@ -44,6 +115,8 @@ end
 function [I2, I3, I4] = matlab_nonuni_illu_remov(I,disk_size, bin_thresh)
     se = strel('disk',disk_size);
     background = imopen(I,se);
+%     LPF = avg_filt(50);
+%     background = conv2(I,LPF,'same');
     %imshow(background);
     I2 = I - background;
     %imshow(I2,[]);
@@ -52,46 +125,7 @@ function [I2, I3, I4] = matlab_nonuni_illu_remov(I,disk_size, bin_thresh)
     bw = imbinarize(I3);
     I4 = bwareaopen(bw,bin_thresh);
 end
-function p = preprocess(I)
 
-
-    
-
-
-    
-
-    HPF = 1 - LPF;
-    %BPF = 1 - (LPF + HPF);       %Band-pass Filter
-    
-    HP_content = conv2(I,HPF,'same');
-    
-%     LP_content = LPF .* fftI;         %relevant frequency regions
-%     HP_content = HPF .* fftI;
-    
-    LP_content = fftshift(fft2(LP_content));
-    HP_content
-    [M, N] = size(I);
-
-    %fftI=fftshift(fft2(I));
-
-    %LPF = mat2gray(fspecial('gaussian', [M N], 15));    %Low-pass Filters
-    %LPF = filter2(fspecial('average',15),I)./255;
-     
-    %LPF=LPF~=0;
-%     LPF2 = mat2gray(fspecial('gaussian', [M N], 5));
-%     LPF2=LPF2~=0;
-%     HPF = 1 - LPF2;                  %High-pass Filter
-%     BPF = 1 - (LPF + HPF);           %Band-pass Filter
-%     %LP_content = LPF .* fftI;        %Partition spectrum into
-%     BP_content = BPF .* fftI;         %relevant frequency regions
-%     HP_content = HPF .* fftI;
-%     HP_content = HP_content .* 0.01;       %Reduce noise by dampening
-    
-    backgrnd = uint8(abs(ifft2(LP_content)));
-    backgrnd(:) = mean(backgrnd(:));
-    LP_content = fftshift(fft2(backgrnd));
-    
-    %high frequency content
-    newSpectrum = LP_content + HP_content;
-    p = uint8(abs(ifft2(newSpectrum)));
+function filt = avg_filt(n)
+    filt = 1/(n^2)*ones(n);
 end
