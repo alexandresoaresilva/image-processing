cfeb581055b6e800a66593c7254b5c9b0d0fe3aa
alexandresoaresilva <alexandre.soares-da-silva@ttu.edu>
2018-11-07 18:51:14 -0600

    crops original images

--- 03. HW3/cam_Capt.m
+++ 03. HW3/cam_Capt.m
@@ -1,7 +1,6 @@
 clc, clear, close all
 % addpath('VLFEATROOT')
-run('VLFEATROOT/toolbox/vl_setup');
-vl_version verbose
+%vl_version verbose
 %for the closing operation
 SELECT_I = 8;
 
@@ -13,6 +12,7 @@ AVG_FILTER_SIZE = 3;
 % webcam_connected = webcam(webcamlist_conn{1});
 % webcam_connected.Resolution ='640x480';
 % 
+run('VLFEATROOT/toolbox/vl_setup');
 % webcam_connected.Resolution = '320x240';
 % prv = preview(webcam_connected);
 % prv.Parent.Visible = 'off';
@@ -27,20 +27,23 @@ AVG_FILTER_SIZE = 3;
 I = imread('fuzzy3_2.jpg');
 digits = extract_digits(I,AVG_FILTER_SIZE);
 
-I2 = imread('IMG_20181102_100540197.jpg');
+I2 = imread('IMG_9584.jpg');
 digits2 = extract_digits(I2,AVG_FILTER_SIZE);
 
 figure;
 sub_plots(digits);
+figure;
+sub_plots(digits2);
 %% sift code
 % run('VLFEATROOT/toolbox/vl_setup');
 % vl_version verbose
 
 [f,d] = vl_sift(single(digits{1}));
-
+figure;
 perm = randperm(size(f,2)) ;
 P = length(perm);
 sel = perm(1:P);
+% figure
 h1 = vl_plotframe(f(:,sel)) ;
 h2 = vl_plotframe(f(:,sel)) ;
 set(h1,'color','k','linewidth',2) ;
@@ -57,8 +60,8 @@ set(h3,'color','g') ;
 
 % subplot(1,2,2)
 % imshow(I2,[]);
-
-[f1,d1] = vl_sift(single(digits2{7}));
+figure;
+[f1,d1] = vl_sift(single(digits2{2}));
 perm = randperm(size(f1,2)) ;
 P = length(perm);
 sel = perm(1:P);
@@ -71,7 +74,6 @@ set(h3,'color','g') ;
 
 [matches, scores] = vl_ubcmatch(d, d1);
 %% functions
-
 %digits is a cell array
 function digits = extract_digits(I,avg_filter_size)
     if length(size(I)) > 2
@@ -83,8 +85,8 @@ function digits = extract_digits(I,avg_filter_size)
     LPF = avg_filt(avg_filter_size);
     % I1 = I;
     I1 = conv2(I,LPF,'valid');
-
-    I_bin = ~imbinarize(uint8(I1),.4);
+    I_bin = I1;
+    I_bin = ~imbinarize(uint8(I1),.5);
     for i=1:20
         I_bin = medfilt2(I_bin);
     end
@@ -104,12 +106,31 @@ function digits = extract_digits(I,avg_filter_size)
     j = 1;
     for i=1:length(I_props)
         if I_props(i).Area > (M*N / 330)
-            if I_props(i).BoundingBox(4) > N/8 %n is height
+%             if I_props(i).BoundingBox(4) > N/8 %n is height
                 rect = rectangle('Position',I_props(i).BoundingBox,'EdgeColor','r','LineWidth',3);
-                digit = I_crop_withBound(I_bin,I_props(i).BoundingBox);
-                digits{j} = I_close(digit,150);
+                
+                [digit_bin, pre, pos] = I_crop_withBound(I_bin,I_props(i).BoundingBox);
+                
+                x_origin = I_props(i).BoundingBox(1)-pre(2);
+                y_origin = I_props(i).BoundingBox(2)-pre(1);
+%                 if x_origin < 0 
+%                     x_origin  = 0;
+%                 end
+%                 if y_origin < 0 
+%                     y_origin  = 0;
+%                 end
+                
+                x_width = I_props(i).BoundingBox(3) + pre(2)+pos(2);
+                y_width = I_props(i).BoundingBox(4) + pre(1)+pos(1);
+                BoundingBox = [x_origin,y_origin,...
+                    x_width, y_width];
+                digit = imcrop(I, BoundingBox);
+
+%                 digit = I_crop_withBound(I,I_props(i).BoundingBox);
+                digits{j} = digit;
+%                 digits{j} = I_close(digit,150);
                 j = j + 1;
-            end
+%             end
         end
     end    
 end
@@ -125,24 +146,25 @@ function digit = I_close(digit, resize_side)
     digit = imresize(digit,[resize_side resize_side]);
 end
 
-function digit = I_crop_withBound(I_bin,boundingBox)
+function [digit, pre, pos] = I_crop_withBound(I_bin,boundingBox)
     digit = imcrop(I_bin, boundingBox);
     %             digits{i} = imcrop(I_binclosed, I_props(i).BoundingBox);
     [m,n] = size(digit);
     [max_x,i] = max([m,n]);            
-    digit = pad_digit_img(digit ,ceil(max_x*1.2));
+    [digit, pre, pos] = pad_digit_img(digit ,ceil(max_x*1.05));
 
     
-    function I = pad_digit_img(I,square_side)
+    function [I, pre, pos] = pad_digit_img(I,square_side)
         [x,y] = size(I);
         [x_pre, x_pos] = padding_pre_post_calc(x,square_side);
         [y_pre, y_pos] = padding_pre_post_calc(y,square_side);
 
-        I = padarray(I,[x_pre y_pre],'pre');
-        I = padarray(I,[x_pos y_pos],'pos');
+        pre = [x_pre y_pre];
+        pos = [x_pos y_pos];
+        I = padarray(I, pre,'pre');
+        I = padarray(I, pos,'pos');
         
         function [n_pre, n_pos] = padding_pre_post_calc(n,dim_size)
-
             n_is_not_even = rem(dim_size-n,2);
             n_pre = (dim_size-n)/2;
     %         n_pre_is_not_even = rem(n_pre,2);
